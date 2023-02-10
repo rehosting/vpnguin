b7631eb3a424c094071866cee8a562d171ac125e
Andrew Fasano <fasano@mit.edu>
2023-02-10 18:42:17 -0500

    It's working

--- src/guest.rs
+++ src/guest.rs
@@ -8,12 +8,14 @@ use anyhow::{Context, Result};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpStream, UdpSocket},
+    time::timeout,
     //task::yield_now,
 };
 //use tokio_vsock::{SockAddr, VsockListener, VsockStream};
 use std::arch::asm;
 use std::io::Cursor;
 use std::mem;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 // DEBUG
 //use crate::HostRequest::Forward;
@@ -86,11 +88,11 @@ pub async fn execute(_command: &Guest) -> Result<()> {
         hypercall(GET_DATA, buffer_addr);
 
         if buffer[0] == 0 {
-            println!("SLEEP 2s");
+            println!("[VPN] SLEEP 2s");
             sleep(Duration::from_millis(2000)).await;
             continue;
         } else if buffer[0] == 1 {
-            println!("RETRY");
+            println!("[VPN] RETRY");
             //yield_now().await;
             continue;
         } else if buffer[0] == 2 {
@@ -99,25 +101,28 @@ pub async fn execute(_command: &Guest) -> Result<()> {
 
         } else if buffer[0] != 0xff {
             hypercall(INPUT_FINISHED, 2); // 2 indicates internal error
-            error!("Unexpected command in hypercall message: {:?}", buffer[0]);
+            error!("[VPN] Unexpected command in hypercall message: {:?}", buffer[0]);
         }
         let cur = Cursor::new(&buffer);
         match bincode::deserialize_from::<_, HyperBuf>(cur) {
             Ok(request) => {
-                println!("Sending data to target: {:?}", request.target);
+                println!("[VPN] Sending data to target: {:?}", request.target);
                 //println!("Request command: {:?}", request.command);
                 //println!("Request data: {:?}", request.payload);
 
                 tokio::spawn(async move {
                     if let Err(e) = process_buffer(request).await {
                         hypercall(INPUT_FINISHED, 1); // 1 indicates error connecting
-                        error!("unable to process client: {e:#?}");
+                        // TODO: distinguish between connection reset (i.e., target didn't like the message) and other codes
+                        // that might indicate that it failed to get the message? Or maybe these
+                        // errors all should just indicate end of processing...?/Wait
+                        error!("[VPN] Unable to process client: {e:#?}");
                     }
                 });
             },
             Err(e) => {
                 hypercall(INPUT_FINISHED, 2); // 2 indicates internal error
-                println!("Error deserializing data from hypercall {:?}", e);
+                println!("[VPN] Error deserializing data from hypercall {:?}", e);
             },
         }
     }
@@ -162,18 +167,28 @@ async fn forward_tcp(
 
     // Send data on stream
     let send_buf = String::from_utf8_lossy(&data);
-    println!("Connecting to {:?} and sending request: {:?}", stream, send_buf);
+    println!("[VPN] {:?} Connecting to {:?} and sending request: {:?}", SystemTime::now(), stream, send_buf);
     stream.write(data).await?;
 
     // Just for debugging, let's try printing any response we get. First 100 bytes only
-    // Read from the current data in the TcpStream TODO: timeout
-    println!("Wait for response");
+    // Read from the current data in the TcpStream
+    println!("[VPN] {:?} Wait for response (tcp)", SystemTime::now());
     let mut rx_bytes = [0u8; 100];
-    let n = stream.read(&mut rx_bytes).await.context("unable to read response")?;
-    let received = String::from_utf8_lossy(&rx_bytes);
-    println!("Got {:?} byte response, ready to shutdown: {:?}", n, received);
+    //let n = stream.read(&mut rx_bytes).await.context("unable to read response")?;
+    let rx = stream.read(&mut rx_bytes);
 
-    hypercall(INPUT_FINISHED, 0);
+    match timeout(Duration::from_millis(999), rx).await { // Wait 0.1s XXX this isn't workign as expected???
+        Err(t) => {
+            //error!("timeout");
+            println!("[VPN] {:?} Timeout - waited for {t}", SystemTime::now());
+            hypercall(INPUT_FINISHED, 3);
+        },
+        Ok(n) => {
+            let received = String::from_utf8_lossy(&rx_bytes);
+            println!("[VPN]  {:?} Got {:?} byte response, ready to shutdown: {:?}", SystemTime::now(), n, received);
+            hypercall(INPUT_FINISHED, 0);
+        }
+    };
     Ok(())
 }
 
@@ -196,12 +211,12 @@ async fn forward_udp(
         .await
         .context("unable to write client datagram")?;
 
-    println!("Wait for response");
+    println!("[VPN] Wait for response (udp XXX response unexpected)");
     let mut rx_bytes = [0u8; 100];
     let n = socket.recv(&mut rx_bytes).await.context("unable to read response")?;
-    println!("Got {:?} byte response, ready to shutdown: {:?}", n, rx_bytes);
+    println!("[VPN] Got {:?} byte response, ready to shutdown: {:?}", n, rx_bytes);
     let received = std::str::from_utf8(&rx_bytes).expect("valid utf8");
-    eprintln!("{}", received);
+    eprintln!("[VPN] {}", received);
 
     hypercall(INPUT_FINISHED, 0);
     Ok(())
