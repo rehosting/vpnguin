76626a6537178c917da83a4cec0bc26feee8d45c
Andrew Fasano <fasano@mit.edu>
2024-03-29 19:11:35 -0400

    Build for mips64

--- package.sh
+++ package.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 set -eux
 
-echo "Building for 4 target arches"
+echo "Building for 5 target arches"
 cargo build --target x86_64-unknown-linux-musl --release
 cargo build --target arm-unknown-linux-musleabi  --release
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --target mips-unknown-linux-musl --release
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --target mipsel-unknown-linux-musl --release
+
+# Mips64 needs us to setup the musl64 toolchain. I'm not sure why gnuabi64 doesn't work for us
+# https://github.com/panda-re/embedded-toolchains/issues/5
+rustup target add mips64-unknown-linux-muslabi64
+RUSTFLAGS='-C target-feature=+crt-static' cargo build --target mips64-unknown-linux-muslabi64 --release
 
 OUT=vpn
 echo "Packaging into ${OUT} and packaging as vpn.tar.gz"
 rm  -rf $OUT
 mkdir -p $OUT
 
+git config --global --add safe.directory /app
 echo "vsock_vpn at $(git rev-parse HEAD) built at $(date)" > $OUT/README.txt
 
 for x in target/*/release/vsock_vpn; do
@@ -28,6 +34,9 @@ for x in target/*/release/vsock_vpn; do
       ;;
     mipsel-unknown-linux-musl)
       SUFFIX="mipsel"
+      ;;
+    mips64-unknown-linux-muslabi64)
+      SUFFIX="mips64eb"
       ;;
     *)
       echo "Unsupported architecture: $ARCH"
