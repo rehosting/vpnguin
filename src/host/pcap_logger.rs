becef6d3295dd124272007a27ad56af324291605
Zak Estrada <zachary.estrada@ll.mit.edu>
2025-04-28 15:25:50 -0400

    pcap: refactored to use builder; fixed up TCP seq+ack

--- src/host/pcap_logger.rs
+++ src/host/pcap_logger.rs
@@ -11,7 +11,7 @@ use std::{
 use tokio::sync::mpsc;
 
 use pcap_file::pcap::{PcapHeader, PcapPacket, PcapWriter};
-use etherparse::{IpNumber, Ipv4Header, Ipv6FlowLabel, Ipv6Header, TcpHeader, UdpHeader, Ethernet2Header, EtherType};
+use etherparse::{PacketBuilder, IpNumber};
 
 // Since TCP is stateful, we need to track this stuff
 #[derive(Hash, Eq, PartialEq, Clone, Debug)]
@@ -26,6 +26,14 @@ struct TcpState {
     next_ack: u32,
     last_payload_len: usize,
     seen_syn: bool,
+}
+
+// Decided to make this a struct so things are more readable
+#[derive(Debug, Clone, Copy, Default)]
+pub struct TcpFlags {
+    pub syn: bool,
+    pub ack: bool,
+    pub fin: bool,
 }
 
 use dashmap::DashMap;
@@ -71,60 +79,50 @@ impl PcapLogger {
     // Since TCP is stateful, we'll fake out sequence numbers as well as SYN stuff
     pub async fn init_tcp_stream(&self, peer_address: SocketAddr, internal_address: SocketAddr) {
         if let Some(tcp_map) = &self.tcp_flows {
-            let internal_to_peer_seq = {
-                let mut internal_to_peer = tcp_map
-                    .entry(TcpFlow {
-                        source: internal_address,
-                        dest: peer_address,
-                    })
-                    .or_insert_with(TcpState::default);
-                let seq = internal_to_peer.next_seq;
-                internal_to_peer.next_seq += 1;
-                seq
-            };
+            // Ensure both directions are initialized
+            tcp_map.entry(TcpFlow {
+                source: internal_address,
+                dest: peer_address,
+            })
+            .or_insert_with(TcpState::default);
 
-            let peer_to_internal_seq = {
-                let mut peer_to_internal = tcp_map
-                    .entry(TcpFlow {
-                        source: peer_address,
-                        dest: internal_address,
-                    })
-                    .or_insert_with(TcpState::default);
-                let seq = peer_to_internal.next_seq;
-                peer_to_internal.next_seq += 1;
-                seq
-            };
+            tcp_map.entry(TcpFlow {
+                source: peer_address,
+                dest: internal_address,
+            })
+            .or_insert_with(TcpState::default);
 
-            // SYN (internal -> peer)
+            // SYN (peer -> internal)
             self.log_packet(
                 &[],
                 Transport::Tcp,
-                internal_address,
                 peer_address,
-                Some((internal_to_peer_seq, 0, true, false)), // SYN flag
+                internal_address,
+                Some(TcpFlags{syn: true, ack: false, fin: false}),
             ).await;
 
-            // SYN+ACK (peer -> internal)
+            // SYN+ACK (internal -> peer)
             self.log_packet(
                 &[],
                 Transport::Tcp,
-                peer_address,
                 internal_address,
-                Some((peer_to_internal_seq, internal_to_peer_seq + 1, true, true)), // SYN and ACK flags
+                peer_address,
+                Some(TcpFlags{syn: true, ack: true, fin: false}),
             ).await;
 
-            // ACK (internal -> peer)
+            // ACK (peer -> internal)
             self.log_packet(
                 &[],
                 Transport::Tcp,
-                internal_address,
                 peer_address,
-                Some((internal_to_peer_seq + 1, peer_to_internal_seq + 1, false, true)), // ACK flag
+                internal_address,
+                Some(TcpFlags{syn: false, ack: true, fin: false}),
             ).await;
         }
     }
 
     pub async fn close_tcp_stream(&self, peer_address: SocketAddr, internal_address: SocketAddr) {
+        // WARNING: this code is untested, we don't really tear down connections in vpnguin
         if let Some(tcp_map) = &self.tcp_flows {
             // Get the TCP states for both directions
             let internal_to_peer = tcp_map.get(&TcpFlow {
@@ -136,37 +134,32 @@ impl PcapLogger {
                 dest: internal_address,
             });
 
-            if let (Some(internal_to_peer), Some(peer_to_internal)) = (internal_to_peer, peer_to_internal) {
-
-                let internal_to_peer_seq  = internal_to_peer.next_seq + 1;
-                let internal_to_peer_next_ack =  internal_to_peer.next_ack;
-                let peer_to_internal_seq= peer_to_internal.next_seq + 1;
-
-                // FIN (internal -> peer)
+            if let (Some(mut internal_to_peer), Some(mut peer_to_internal)) = (internal_to_peer, peer_to_internal) {
+                // FIN (peer -> internal)
                 self.log_packet(
                     &[],
                     Transport::Tcp,
-                    internal_address,
                     peer_address,
-                    Some((internal_to_peer_seq, internal_to_peer_next_ack, true, false)),
+                    internal_address,
+                    Some(TcpFlags{syn: false, ack: false, fin: true}),
                 ).await;
 
-                // FIN+ACK (peer -> internal)
+                // FIN+ACK (internal -> peer)
                 self.log_packet(
                     &[],
                     Transport::Tcp,
-                    peer_address,
                     internal_address,
-                    Some((peer_to_internal_seq, internal_to_peer_seq + 1, true, true)),
+                    peer_address,
+                    Some(TcpFlags{syn: false, ack: true, fin: true}),
                 ).await;
 
-                // ACK (internal -> peer)
+                // ACK (peer -> internal)
                 self.log_packet(
                     &[],
                     Transport::Tcp,
-                    internal_address,
                     peer_address,
-                    Some((internal_to_peer_seq + 1, peer_to_internal_seq + 1, false, true)),
+                    internal_address,
+                    Some(TcpFlags{syn: false, ack: true, fin: false}),
                 ).await;
 
                 // Clean up the map
@@ -188,109 +181,91 @@ impl PcapLogger {
         proto: Transport,
         src: SocketAddr,
         dest: SocketAddr,
-        tcp_flags: Option<(u32, u32, bool, bool)>,
+        flags: Option<TcpFlags>,
     ) {
-        let (transport_len, ip_number): (u16, IpNumber) = match proto {
-            Transport::Tcp => {
-                ((20 + payload.len()).try_into().unwrap(), IpNumber::TCP)
-            }
-            Transport::Udp => {
-                ((8 + payload.len()).try_into().unwrap(), IpNumber::UDP)
-            }
-        };
-
         let mut buf: Vec<u8> = Vec::new();
 
         // TODO: grab MACs from Host and Guest
-        let eth = Ethernet2Header {
-            source: [0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff],
-            destination: [0x11, 0x22, 0x33, 0x44, 0x55, 0x66],
-            ether_type: match (src.ip(), dest.ip()) {
-                (IpAddr::V4(_), IpAddr::V4(_)) => EtherType::IPV4,
-                (IpAddr::V6(_), IpAddr::V6(_)) => EtherType::IPV6,
-                _ => {
-                    error!("Unable to parse packet from {} to {}", src, dest);
-                    return;
-                }
-            },
-        };
-        eth.write(&mut buf).unwrap();
+        let builder = PacketBuilder::ethernet2(
+            [0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff],
+            [0x11, 0x22, 0x33, 0x44, 0x55, 0x66],
+        );
 
-        let mut ip_header = Vec::with_capacity(60);
-        match (src.ip(), dest.ip()) {
-            (IpAddr::V4(src_ip), IpAddr::V4(dest_ip)) => {
-                let bytes = Ipv4Header::new(
-                    transport_len,
-                    64,
-                    ip_number,
-                    src_ip.octets(),
-                    dest_ip.octets(),
-                )
-                .unwrap()
-                .to_bytes();
-                ip_header.extend_from_slice(&bytes);
-            }
-            (IpAddr::V6(src_ip), IpAddr::V6(dest_ip)) => {
-                let bytes = Ipv6Header {
-                    traffic_class: 0,
-                    flow_label: Ipv6FlowLabel::ZERO,
-                    payload_length: transport_len,
-                    next_header: ip_number,
-                    hop_limit: 64,
-                    source: src_ip.octets(),
-                    destination: dest_ip.octets(),
-                }
-                .to_bytes();
-                ip_header.extend_from_slice(&bytes);
-            }
+        let builder = match (src.ip(), dest.ip()) {
+            (IpAddr::V4(src_ip), IpAddr::V4(dest_ip)) => builder.ipv4(
+                src_ip.octets(),
+                dest_ip.octets(),
+                64, // TTL
+            ),
+            (IpAddr::V6(src_ip), IpAddr::V6(dest_ip)) => builder.ipv6(
+                src_ip.octets(),
+                dest_ip.octets(),
+                64, // Hop limit
+            ),
             _ => {
-                error!("Unable to parse packet from {} to {}", src, dest);
+                error!("Source and destination IP versions do not match");
                 return;
             }
         };
-        buf.extend_from_slice(&ip_header);
 
         match proto {
             Transport::Tcp => {
-                let (mut seq , ack, syn, ack_flag) = tcp_flags.unwrap_or((0, 0, false, false));
+                let mut seq = 0;
+                let mut ack = 0;
+
                 if let Some(tcp_map) = &self.tcp_flows {
+                    // Fetch and update the sequence number for the current flow
                     if let Some(mut state) = tcp_map.get_mut(&TcpFlow { source: src, dest }) {
-                        if seq == 0 { seq = state.next_seq; } // Not rusty, sorry
-                        state.next_seq += payload.len() as u32;
+                        seq = state.next_seq;
+                        // If SYN or FIN, we need to add 1 (payload.len is 0)
+                        state.next_seq += payload.len() as u32 + if flags.as_ref().map_or(false, |f| f.syn || f.fin) { 1 } else { 0 };
+                    }
+
+                    // Fetch and update the acknowledgment number for the opposite flow
+                    if let Some(mut opposite_state) = tcp_map.get_mut(&TcpFlow { source: dest, dest: src }) {
+                        ack = opposite_state.next_seq;
+                        if flags.map_or(false, |f| f.ack) {
+                            opposite_state.next_ack = seq;
+                        }
                     }
                 }
 
-                let mut tcp_header = TcpHeader::new(src.port(), dest.port(), seq, 0);
-                tcp_header.acknowledgment_number = ack;
-                tcp_header.syn = syn;
-                tcp_header.ack = ack_flag;
-                let checksum = match src.ip() {
-                    IpAddr::V4(_) => tcp_header.calc_checksum_ipv4(&Ipv4Header::from_slice(&ip_header).unwrap().0, payload),
-                    IpAddr::V6(_) => tcp_header.calc_checksum_ipv6(&Ipv6Header::from_slice(&ip_header).unwrap().0, payload)
-                }.unwrap();
-                tcp_header.checksum = checksum;
-                buf.extend_from_slice(&tcp_header.to_bytes());
+                let builder = builder
+                    .tcp(src.port(), dest.port(), seq, 0)
+                    .apply_if(flags.as_ref().map_or(false, |f| f.syn), |b| b.syn())
+                    .apply_if(flags.as_ref().map_or(false, |f| f.ack), |b| b.ack(ack))
+                    .apply_if(flags.as_ref().map_or(false, |f| f.fin), |b| b.fin());
+
+                builder.write(&mut buf, payload).unwrap();
             }
             Transport::Udp => {
-                let udp_header = match src.ip() {
-                    IpAddr::V4(_) => UdpHeader::with_ipv4_checksum(
-                        src.port(),
-                        dest.port(),
-                        &Ipv4Header::from_slice(&ip_header).unwrap().0,
-                        payload,
-                    ),
-                    IpAddr::V6(_) => UdpHeader::with_ipv6_checksum(
-                        src.port(),
-                        dest.port(),
-                        &Ipv6Header::from_slice(&ip_header).unwrap().0,
-                        payload,
-                    ),
-                };
-                buf.extend_from_slice(&udp_header.unwrap().to_bytes());
+                let builder = builder.udp(src.port(), dest.port());
+                builder.write(&mut buf, payload).unwrap();
             }
         }
-        buf.extend_from_slice(payload);
 
         self.log(&buf).await;
+    }
+}
+
+// This extension courtesy of copilot
+trait PacketBuilderExt {
+    fn apply_if<F>(self, condition: bool, f: F) -> Self
+    where
+        F: FnOnce(Self) -> Self,
+        Self: Sized;
+}
+
+impl<T> PacketBuilderExt for T {
+    fn apply_if<F>(self, condition: bool, f: F) -> Self
+    where
+        F: FnOnce(Self) -> Self,
+        Self: Sized,
+    {
+        if condition {
+            f(self)
+        } else {
+            self
+        }
     }
 }
